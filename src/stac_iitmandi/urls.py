928c277b55f7281d197bb08b0311d4f06c86b7c8
Gajraj Singh Chouhan <54475046+gajrajgchouhan@users.noreply.github.com>
2020-10-28 19:03:17 +0530

    Added template! (#138)

--- src/stac_iitmandi/urls.py
+++ src/stac_iitmandi/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='stac-home'),
+    path('template.html', views.template, name='template'),
 ]
--- src/stac_iitmandi/views.py
+++ src/stac_iitmandi/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'stac_iitmandi/home.html')
+
+def template(request):
+    return render(request, 'stac_iitmandi/template.html')
