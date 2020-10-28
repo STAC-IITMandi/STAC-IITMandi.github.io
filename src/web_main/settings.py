e02f4408df74f598b16a8e0075612e0a4536c18f
Gajraj Singh Chouhan <54475046+gajrajgchouhan@users.noreply.github.com>
2020-10-28 19:03:17 +0530

    Added template! (#138)

--- src/web_main/settings.py
+++ src/web_main/settings.py
@@ -54,10 +54,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'web_main.urls'
 
+TEMPLATE_DIR = (
+        os.path.join(BASE_DIR, 'stac_iitmandi', 'templates', 'stac_iitmandi/')
+    )
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
