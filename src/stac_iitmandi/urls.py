cc1a862b548a29d9afedbf4920f288bb1e5a8da7
tusharrao198 <tushar.rao198@gmail.com>
2020-11-07 22:17:25 +0530

    1. added new pages,2. modified navbar,3. Added stac logo 4. Title added

--- src/stac_iitmandi/urls.py
+++ src/stac_iitmandi/urls.py
@@ -4,4 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='stac-home'),
     path('template.html', views.template, name='template'),
+    path('astrax.html', views.astrax, name = 'astrax'),
+    path('zenith.html', views.zenith, name = 'zenith'),
+    path('utkarsh.html', views.utkarsh, name = 'utkarsh'),
+    path('about.html', views.about, name = 'about'),
+    path('alumni_s.html', views.alumni_s, name = 'alumni_s'),
+    path('photogallery.html', views.photogallery, name = 'photogallery'),
+    path('videogallery.html', views.videogallery, name = 'videogallery'),
 ]
--- src/stac_iitmandi/views.py
+++ src/stac_iitmandi/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'stac_iitmandi/home.html')
 
+# rendering home page 
+def home(request):
+    return render(request, 'stac_iitmandi/home.html' , {'title': 'Home'})
+# rendering template page
 def template(request):
     return render(request, 'stac_iitmandi/template.html')
+# rendering ASTRAX page
+def astrax(request):
+    return render(request, 'stac_iitmandi/astrax.html', {'title': 'Astrax'})
+# rendering Utkarsh page
+def utkarsh(request):
+    return render(request, 'stac_iitmandi/utkarsh.html', {'title': 'Utkarsh'})
+# rendering Zenith page
+def zenith(request):
+    return render(request, 'stac_iitmandi/zenith.html', {'title': 'Zenith'})
+# rendering Alumni's page    
+def alumni_s(request):
+    return render(request, "stac_iitmandi/alumni_s.html", {'title': "Alumni's"})
+# rendering About page
+def about(request):
+    return render(request, 'stac_iitmandi/about.html', {'title': 'About'})
+# rendering Photogallery page
+def photogallery(request):
+    return render(request, 'stac_iitmandi/photogallery.html', {'title': 'Photos'})
+#rendering videogallery page
+def videogallery(request):
+    return render(request, 'stac_iitmandi/videogallery.html', {'title': 'Videos'})
