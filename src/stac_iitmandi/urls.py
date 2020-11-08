20acc507ef3b42a84041e2f1528b344ab253b7de
Tushar Rao <56690827+tusharrao198@users.noreply.github.com>
2020-11-08 11:04:58 +0530

    Added new pages and stac logo. (#149)
    Changes:
    - added new pages
    - modified navbar
    - Added stac logo
    - Title added

--- src/stac_iitmandi/urls.py
+++ src/stac_iitmandi/urls.py
@@ -4,4 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='stac-home'),
     path('template.html', views.template, name='template'),
+    path('astrax.html', views.astrax, name = 'astrax'),
+    path('zenith.html', views.zenith, name = 'zenith'),
+    path('utkarsh.html', views.utkarsh, name = 'utkarsh'),
+    path('about.html', views.about, name = 'about'),
+    path('alumni.html', views.alumni, name = 'alumni'),
+    path('photogallery.html', views.photogallery, name = 'photogallery'),
+    path('videogallery.html', views.videogallery, name = 'videogallery'),
 ]
