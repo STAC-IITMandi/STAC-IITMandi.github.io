ed8b7214d8accae99ae8cd5cd87e466c6c643904
Tushar Rao <56690827+tusharrao198@users.noreply.github.com>
2020-11-09 23:06:57 +0530

    active page bug fixed  (#152)
    Changes
    - active page bug fixed
    - added .active tag in style.css
    - style.css moved to css folder
    - removed bg-custom class from style.css
    Fixes #151.

--- src/stac_iitmandi/views.py
+++ src/stac_iitmandi/views.py
@@ -3,28 +3,28 @@ from django.shortcuts import render
 
 # rendering home page 
 def home(request):
-    return render(request, 'stac_iitmandi/home.html' , {'title': 'Home'})
+    return render(request, 'stac_iitmandi/home.html' , {'title': 'Home' ,'home':'active'})
 # rendering template page
 def template(request):
     return render(request, 'stac_iitmandi/template.html')
 # rendering ASTRAX page
 def astrax(request):
-    return render(request, 'stac_iitmandi/astrax.html', {'title': 'Astrax'})
+    return render(request, 'stac_iitmandi/astrax.html', {'title': 'Astrax','astrax':'active', 'events_':"active"})
 # rendering Utkarsh page
 def utkarsh(request):
-    return render(request, 'stac_iitmandi/utkarsh.html', {'title': 'Utkarsh'})
+    return render(request, 'stac_iitmandi/utkarsh.html', {'title': 'Utkarsh' ,'utkarsh':'active','events_':"active"})
 # rendering Zenith page
 def zenith(request):
-    return render(request, 'stac_iitmandi/zenith.html', {'title': 'Zenith'})
+    return render(request, 'stac_iitmandi/zenith.html', {'title': 'Zenith' ,'zenith':'active', 'events_':"active"})
 # rendering Alumni page    
 def alumni(request):
-    return render(request, "stac_iitmandi/alumni.html", {'title': "Alumni"})
+    return render(request, "stac_iitmandi/alumni.html", {'title': "Alumni" ,'alumni':'active'})
 # rendering About page
 def about(request):
-    return render(request, 'stac_iitmandi/about.html', {'title': 'About'})
+    return render(request, 'stac_iitmandi/about.html', {'title': 'About' ,'about':'active'})
 # rendering Photogallery page
 def photogallery(request):
-    return render(request, 'stac_iitmandi/photogallery.html', {'title': 'Photos'})
+    return render(request, 'stac_iitmandi/photogallery.html', {'title': 'Photos' ,'photos':'active','gallery_':"active"})
 #rendering videogallery page
 def videogallery(request):
-    return render(request, 'stac_iitmandi/videogallery.html', {'title': 'Videos'})
+    return render(request, 'stac_iitmandi/videogallery.html', {'title': 'Videos' ,'videos':'active','gallery_':"active"})
