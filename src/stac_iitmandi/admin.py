958f528c561a71f3da53dfef49e18f8f2c796f40
harnaman-hk <harnaman24@gmail.com>
2020-12-10 20:50:16 +0530

    flake8 formatting

--- src/stac_iitmandi/admin.py
+++ src/stac_iitmandi/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import core_team , coordinators
+from .models import core_team, coordinators
 
 # register models
 admin.site.register(core_team)
 admin.site.register(coordinators)
- 
--- src/stac_iitmandi/views.py
+++ src/stac_iitmandi/views.py
@@ -1,36 +1,89 @@
 from django.shortcuts import render
 from .models import core_team as members
-from .models import coordinators 
+from .models import coordinators
 
-# rendering home page 
+
+# rendering home page
 def home(request):
-    return render(request, 'stac_iitmandi/home.html' , {'title': 'Home' ,'home':'active'})
+    return render(
+        request, "stac_iitmandi/home.html", {"title": "Home", "home": "active"}
+    )
+
+
 # rendering template page
 def template(request):
-    return render(request, 'stac_iitmandi/template.html')
+    return render(request, "stac_iitmandi/template.html")
+
+
 # rendering ASTRAX page
 def astrax(request):
-    return render(request, 'stac_iitmandi/astrax.html', {'title': 'Astrax','astrax':'active', 'events_':"active"})
+    return render(
+        request,
+        "stac_iitmandi/astrax.html",
+        {"title": "Astrax", "astrax": "active", "events_": "active"},
+    )
+
+
 # rendering Utkarsh page
 def utkarsh(request):
-    return render(request, 'stac_iitmandi/utkarsh.html', {'title': 'Utkarsh' ,'utkarsh':'active','events_':"active"})
+    return render(
+        request,
+        "stac_iitmandi/utkarsh.html",
+        {"title": "Utkarsh", "utkarsh": "active", "events_": "active"},
+    )
+
+
 # rendering Zenith page
 def zenith(request):
-    return render(request, 'stac_iitmandi/zenith.html', {'title': 'Zenith' ,'zenith':'active', 'events_':"active"})
-# rendering Alumni page    
+    return render(
+        request,
+        "stac_iitmandi/zenith.html",
+        {"title": "Zenith", "zenith": "active", "events_": "active"},
+    )
+
+
+# rendering Alumni page
 def alumni(request):
-    return render(request, "stac_iitmandi/alumni.html", {'title': "Alumni" ,'alumni':'active'})
+    return render(
+        request,
+        "stac_iitmandi/alumni.html",
+        {"title": "Alumni", "alumni": "active"}
+    )
+
+
 # rendering About page
 def about(request):
-    return render(request, 'stac_iitmandi/about.html', {'title': 'About' ,'about':'active'})
+    return render(
+        request,
+        "stac_iitmandi/about.html",
+        {"title": "About", "about": "active"}
+    )
+
+
 # rendering Photogallery page
 def photogallery(request):
-    return render(request, 'stac_iitmandi/photogallery.html', {'title': 'Photos' ,'photos':'active','gallery_':"active"})
-#rendering videogallery page
+    return render(
+        request,
+        "stac_iitmandi/photogallery.html",
+        {"title": "Photos", "photos": "active", "gallery_": "active"},
+    )
+
+
+# rendering videogallery page
 def videogallery(request):
-    return render(request, 'stac_iitmandi/videogallery.html', {'title': 'Videos' ,'videos':'active','gallery_':"active"})
+    return render(
+        request,
+        "stac_iitmandi/videogallery.html",
+        {"title": "Videos", "videos": "active", "gallery_": "active"},
+    )
+
+
 # rendering team page
 def team(request):
-    context_ = {'members': members.objects.all().order_by('batch'), 'coordinators':coordinators.objects.all().order_by('batch'), 'title': 'Team' ,'team':'active'}
-    return render(request, 'stac_iitmandi/team.html', context_ )
-    
+    context_ = {
+        "members": members.objects.all().order_by("batch"),
+        "coordinators": coordinators.objects.all().order_by("batch"),
+        "title": "Team",
+        "team": "active",
+    }
+    return render(request, "stac_iitmandi/team.html", context_)
