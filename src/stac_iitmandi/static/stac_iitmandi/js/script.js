61fc1c1fedfd640d2ea5df9dc780208c88d48021
Tushar Rao <56690827+tusharrao198@users.noreply.github.com>
2020-12-14 10:05:08 +0530

    Fixed Navbar Issue.  (#167)
    * 1.Fixed Navbar Issue. 2. Added animations on Navbar 3. Updated team_style.css
    
    * updated style.css
    
    * 1.Fixed STAC log navbar issue in mobile-view 2. removed template.html 3. changes made to style.css
    
    * 1. updated script.js 2. added jquery libraries

--- src/stac_iitmandi/static/stac_iitmandi/js/script.js
+++ src/stac_iitmandi/static/stac_iitmandi/js/script.js
@@ -1,6 +1,33 @@
 $(function () {
-    $(document).scroll(function () {
-        var $nav = $("fixed-top");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+
+    $('.navbar-toggler').on('click', function() {
+        if (!$('#mainNav').hasClass('navbar-reduce')) {
+          $('#mainNav').addClass('navbar-reduce');
+        }
     });
-});
+    
+    /*--/ Navbar Menu Reduce /--*/
+    $(window).trigger("scroll");
+    $(window).on("scroll", function() {
+        var pixels =50;
+        var top = 1200;
+        if ($(window).scrollTop() > pixels) {
+            $(".navbar-expand-md").addClass("bg-light");
+            $(".navbar-expand-md").addClass("navbar-reduce");
+            $(".navbar-expand-md").removeClass("navbar-trans");
+            $('.dropdown-menu').removeClass('dropdown-trans');
+            $('.dropdown-menu').addClass('dropdown-reduce');
+
+        } else {
+            if (!$("#navbarDefault").hasClass("show")) {
+                $(".navbar-expand-md").removeClass("bg-light");
+                $(".navbar-expand-md").removeClass("navbar-reduce");
+                $('.dropdown-menu').removeClass('dropdown-reduce');
+                
+            }
+            $(".navbar-expand-md").addClass("navbar-trans");
+            $('.dropdown-menu').addClass('dropdown-trans');
+        }
+    });
+});(jQuery);
+
