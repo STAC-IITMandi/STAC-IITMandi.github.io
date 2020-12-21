af744305290344d1a6d265627cd601a28bce7a94
tusharrao198 <tushar.rao198@gmail.com>
2020-12-21 16:41:05 +0530

    1. Added club_activities and homepage models 2. Updated Homepage 3. Added Ckeditor in django admin section

--- src/stac_iitmandi/models.py
+++ src/stac_iitmandi/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from PIL import Image
+from ckeditor.fields import RichTextField
 
 
 class core_team(models.Model):
@@ -42,5 +43,43 @@ class coordinators(models.Model):
         img = Image.open(self.image.path)
         if img.height > 240 or img.width > 240:
             output_size = (240, 240)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+
+#####  For Homepage ######
+class club_activity(models.Model):
+    activity = models.CharField(max_length=50, unique=True)
+    content = RichTextField(blank=True, null=True)
+    image = models.ImageField(default="default.jpg", upload_to="Homepage/club_activity")
+
+    def __str__(self):
+        return self.activity
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+        if img.height > 1280 or img.width > 720:
+            output_size = (1280, 720)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+
+# future projects, competetions and events_intro on homepage in one table #
+class homepage(models.Model):
+    topic = models.CharField(max_length=50, unique=True)
+    description = RichTextField(blank=True, null=True)
+    image = models.ImageField(default="default.jpg", upload_to="Homepage")
+
+    def __str__(self):
+        return self.topic
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+        if img.height > 1280 or img.width > 720:
+            output_size = (1280, 720)
             img.thumbnail(output_size)
             img.save(self.image.path)
