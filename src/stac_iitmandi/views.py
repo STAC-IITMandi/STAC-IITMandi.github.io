70a893a308dcb829f3c9ae1d9d8d0659f5735eac
tusharrao198 <tushar.rao198@gmail.com>
2021-02-08 00:36:22 +0530

    updated models ORM

--- src/stac_iitmandi/views.py
+++ src/stac_iitmandi/views.py
@@ -19,13 +19,12 @@ from .models import (
 
 # rendering home page
 def home(request):
-    introduction = homepage.objects.filter(id__in=(15,))
     context_ = {
         "club_activity": club_activity.objects.all(),
         "competitions": homepage.objects.filter(id__in=(1, 2, 3, 4, 8)),
         "events_intro": homepage.objects.filter(id__in=(6, 5, 7)),
         "future_projects": homepage.objects.filter(id__in=(11, 10, 9, 12, 13, 14)),
-        "introduction": introduction[0].description,
+        "introduction": homepage.objects.get(id__in=(15,)),
         "home": "active",
         "achievements": achievements.objects.all(),
         # "astrax": Links.objects.ffilter(linkname="astrax"),
@@ -39,53 +38,49 @@ def home(request):
 
 # rendering ASTRAX page
 def astrax(request):
-    astrax_intro = homepage.objects.filter(id__in=(6,))
     context_ = {
         "events": Astrax.objects.all().exclude(id__in=(6, 1, 2, 3, 4)).order_by("-id"),
         "about_astrax": Astrax.objects.filter(id__in=(1, 2, 3, 4)).order_by("-id"),
         "title": "Astrax",
         "astrax": "active",
         "events_": "active",
-        "astrax_intro": astrax_intro[0].description,
+        "astrax_intro": homepage.objects.get(id__in=(6,)),
     }
     return render(request, "stac_iitmandi/astrax.html", context_)
 
 
 # rendering Utkarsh page
 def utkarsh(request):
-    utkarsh_intro = utkarshEvents.objects.filter(id__in=(4,))
     context_ = {
         "events": utkarshEvents.objects.all().exclude(id__in=(4,)).order_by("-id"),
         "title": "Utkarsh",
         "utkarsh": "active",
         "events_": "active",
-        "utkarsh_intro": utkarsh_intro[0].description,
+        "utkarsh_intro": utkarshEvents.objects.get(id__in=(4,)),
     }
     return render(request, "stac_iitmandi/utkarsh.html", context_)
 
 
 # rendering Zenith page
 def zenith(request):
-    zenith_intro = zenithEvents.objects.filter(id__in=(10,))
     context_ = {
         "events": zenithEvents.objects.all().exclude(id__in=(10,)).order_by("-id"),
         "title": "Zenith",
         "zenith": "active",
         "events_": "active",
-        "zenith_intro": zenith_intro[0].description,
+        "zenith_intro": zenithEvents.objects.get(id__in=(10,)),
     }
     return render(request, "stac_iitmandi/zenith.html", context_)
 
 
 # rendering pleiades page
 def pleiades(request):
-    pleiades_intro = Pleiades.objects.filter(id__in=(1,))
     context_ = {
         "events": Pleiades.objects.all().exclude(id__in=(1,)).order_by("-id"),
         "title": "Pleiades",
         "pleiades": "active",
         "events_": "active",
-        "pleiades_intro": pleiades_intro[0].description,
+        "pleiades_intro": Pleiades.objects.get(id__in=(1,)),
     }
     return render(request, "stac_iitmandi/pleiades.html", context_)
 
@@ -102,11 +97,10 @@ def alumni(request):
 
 # rendering About page
 def about(request):
-    about_intro = About.objects.filter(id__in=(1,))
     context_ = {
         "title": "About",
         "about": "active",
-        "about_intro": about_intro[0].description,
+        "about_intro": About.objects.get(id__in=(1,)),
     }
     return render(request, "stac_iitmandi/about.html", context_)
 
