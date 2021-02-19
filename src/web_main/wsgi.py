c5247a8f980fe1d2a0fb13898d876cb1b28b8a50
gajrajgchouhan <gajrajgchouhan@gmail.com>
2021-02-19 21:17:26 +0530

    added system path

--- src/web_main/wsgi.py
+++ src/web_main/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_main.settings")
+sys.path.append('~/STAC-IITMandi.github.io/src/') # path/src
+sys.path.append('~/STAC-IITMandi.github.io/src/web_main') # path/src/web_main
 
 application = get_wsgi_application()
