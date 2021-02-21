f3985e3df998bbba19feb9125b93997e48ee280e
Gajraj Singh Chouhan <54475046+gajrajgchouhan@users.noreply.github.com>
2021-02-21 21:43:30 +0530

    Fixed Media Path (#198)
    * added system path
    
    * fixed media root path
    
    * removed line break

--- src/web_main/urls.py
+++ src/web_main/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     path("", include("stac_iitmandi.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += static(  # only works in DEBUG mode
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
